08a2fdd4d1be575454bbf6b145de07e0fb379a7e
Peter Judge <=>
2023-01-14 21:47:44 -0600

    added html/css for date-scroll buttons

--- ui/src/components/Date.js
+++ ui/src/components/Date.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import HabitWeek from './HabitWeek';
 
-function Date({date}) {
+function Date({date, leftButton, rightButton}) {
+
+    if (leftButton) {
+        return (
+            <div className="date">
+                <i className="arrow left"></i>
+                <p id={`date-component-${date}`}>
+                    {date}
+                </p>
+            </div>
+        )
+    }
+    if (rightButton) {
+        return (
+            <div className="date">
+                <p id={`date-component-${date}`}>
+                    {date}
+                </p>
+                <i className="arrow right"></i>
+            </div>
+
+        )
+    }
     return (
         <div className="date">
+            {leftButton}
             <p id={`date-component-${date}`}>
                 {date}
             </p>
--- ui/src/components/HabitWeek.js
+++ ui/src/components/HabitWeek.js
@@ -9,6 +9,7 @@ function HabitWeek({toggleHabitDay, habit, dates}) {
     return (
         <div className="habit-week">
             <p className="habit-week-title">{habit.name}</p>
+            
             {dates.map((date, i) => 
                 <HabitDay 
                     toggleHabitDay = {toggleHabitDay}
@@ -16,6 +17,7 @@ function HabitWeek({toggleHabitDay, habit, dates}) {
                     habit={habit}
                     completed={habit.dates_accomp.includes(date)} 
                     key={i}/>)}
+            
         </div>
     )
 }
